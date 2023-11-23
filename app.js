b1c4d55822f1c2671aa8c3d2ccf8d49f1c1a5c95
nagasritha <eernagasritha@gmail.com>
2023-11-23 03:07:08 +0000

    courier details added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const dbPath = path.join(__dirname, "language.db");
+const dbPath = path.join(__dirname, "courier.db");
 const cors = require("cors");
 
 const app = express();
@@ -26,33 +26,9 @@ const initalizingServerAndDb = async () => {
 };
 
 initalizingServerAndDb();
-
-//to create users table
-app.get("/create", async (request, response) => {
-  const query = `
-    CREATE TABLE users(
-        id INTEGER NOT NULL PRIMARY KEY,
-        username VARCHAR(255),
-        email VARCHAR(250),
-        gender VARCHAR(250),
-        location VARCHAR(250),
-        password VARCHAR(250),
-        score INT
-        );`;
-  const creationResponse = await database.run(query);
-  response.send("Table created");
-});
-//to delete the table
-app.delete("/drop/:id", async (request, response) => {
-  const { id } = request.params;
-  const query = `
-    DELETE FROM users WHERE id=${id} `;
-  const queryResult = await database.run(query);
-  response.send("table droped");
-});
 //to register as a user
 app.post("/register", async (request, response) => {
-  const { username, password, email, gender, location } = request.body;
+  const { username, password, email, admin } = request.body;
   if (password.length <= 4) {
     response.status(400);
     response.send("password must contain more than 4 characters");
@@ -62,14 +38,13 @@ app.post("/register", async (request, response) => {
   const query1 = `
     SELECT * FROM users WHERE email="${email}" AND username="${username}";`;
   const query1Result = await database.all(query1);
-  console.log(query1Result.length !== 0);
   if (query1Result.length !== 0) {
     response.send({ message: "User already registered", status: 400 });
     response.status(400);
   } else {
     const query2 = `
-        INSERT INTO users(username,password,email,gender,location,score)
-        VALUES("${username}","${encryptedPassword}","${email}","${gender}","${location}",0)`;
+        INSERT INTO users(username,password,email,admin)
+        VALUES("${username}","${encryptedPassword}","${email}","${admin}")`;
     const query2Result = await database.run(query2);
     response.send({ message: "user added successfully", status: 200 });
   }
@@ -124,38 +99,157 @@ const middleware = async (request, response, next) => {
   }
 };
 //to add the image
-app.get("/galery", async (request, response) => {
-  try {
-    const query1 = `CREATE TABLE images(
-    id INTEGER NOT NULL PRIMARY KEY,
-    image_url VARCHAR(500)
-);`;
-    await database.run(query1);
-    response.send("table created");
-  } catch (error) {
-    response.send(`error:${error}`);
-    response.status(400);
-  }
-});
 
-app.post("/add-image", async (request, response) => {
-  const { imageUrl } = request.body;
-  console.log(imageUrl);
-  const query = `INSERT INTO images (image_url) VALUES("${imageUrl}");`;
-  await database.run(query);
-  response.send({ message: "Data added successfully" });
-});
-
-app.get("/images", middleware, async (request, response) => {
-  const query1 = `SELECT * FROM images;`;
+app.get("/user/:id", async (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  const query1 = `SELECT * FROM users WHERE id=${id};`;
   const data = await database.all(query1);
   response.send(data);
 });
 
-app.delete("/delete-image/:id", async (request, response) => {
+//to insert the data of courier product
+app.post("/add-product", middleware, async (request, response) => {
+  const { username } = request;
+  const {
+    productName,
+    imageUrl,
+    startingPoint,
+    endingPoint,
+    orderDate,
+  } = request.body;
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    const query2 = `INSERT INTO product("product_name","image_url","starting_point","ending_point",order_date)
+   VALUES("${productName}","${imageUrl}","${startingPoint}","${endingPoint}","${orderDate}");`;
+    await database.run(query2);
+    response.send({ message: "Data added", status: 200 });
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+app.post("/event", middleware, async (request, response) => {
+  const { username } = request;
+  const { location, deliverBy, productId } = request.body;
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    console.log(location, productId);
+    const query3 = `SELECT * FROM events WHERE location="${location}" AND product_id=${productId};`;
+    const query3Response = await database.get(query3);
+    console.log(query3Response);
+    if (query3Response !== undefined) {
+      response.send({ message: "location already added", status: 400 });
+    } else {
+      const query2 = `INSERT INTO events("location",deliver_by,product_id)
+   VALUES("${location}","${deliverBy}",${productId});`;
+      await database.run(query2);
+      response.send({ message: "Data added", status: 200 });
+    }
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+//to get the product
+app.get("/product/:id", middleware, async (request, response) => {
+  const { id } = request.params;
+  const query1 = `SELECT * FROM product WHERE id=${id}`;
+  const query2 = `SELECT * FROM events WHERE product_id=${id}`;
+  const response1 = await database.get(query1);
+  const response2 = await database.all(query2);
+  response.send({ response1, eventDetails: response2 });
+});
+
+app.put("/add-product/:id", middleware, async (request, response) => {
+  const { username } = request;
+  const { id } = request.params;
+  const {
+    productName,
+    imageUrl,
+    startingPoint,
+    endingPoint,
+    orderDate,
+  } = request.body;
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    const query2 = `UPDATE product
+    SET product_name="${productName}",
+    image_url="${imageUrl}",
+    starting_point="${startingPoint}",
+    ending_point="${endingPoint}",
+    order_date="${orderDate}"
+    WHERE id=${id};`;
+    await database.run(query2);
+    response.send({ message: "Data updated", status: 200 });
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+app.put("/event/:id", middleware, async (request, response) => {
+  const { username } = request;
+  const { id } = request.params;
+  const { location, deliverBy, productId } = request.body;
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    console.log(location, productId);
+    const query3 = `SELECT * FROM events WHERE location="${location}" AND product_id=${productId} AND deliver_by="${deliverBy}";`;
+    const query3Response = await database.get(query3);
+    console.log(query3Response);
+    if (query3Response !== undefined) {
+      response.send({ message: "location already added", status: 400 });
+    } else {
+      const query2 = `UPDATE events
+      SET location="${location}",
+      deliver_by="${deliverBy}",
+      product_id=${productId}
+      WHERE id=${id}`;
+      await database.run(query2);
+      response.send({ message: "Data updated", status: 200 });
+    }
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+app.delete("/event/:id", middleware, async (request, response) => {
+  const { username } = request;
+  const { id } = request.params;
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    const query2 = `DELETE FROM events WHERE id=${id};`;
+    await database.run(query2);
+    response.send({ message: "event deleted", status: 200 });
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+app.delete("/product/:id", middleware, async (request, response) => {
+  const { username } = request;
   const { id } = request.params;
   console.log(id);
-  const query = `DELETE FROM images WHERE id=${id};`;
-  await database.run(query);
-  response.send("Item deleted");
+  const query1 = `SELECT * FROM users WHERE username="${username}";`;
+  const query1Response = await database.get(query1);
+  if (query1Response.admin === "True") {
+    const query2 = `DELETE FROM product WHERE id=${id};`;
+    await database.run(query2);
+    const query3 = `DELETE FROM events WHERE product_id=${id};`;
+    await database.run(query3);
+    response.send({ message: "event deleted", status: 200 });
+  } else {
+    response.send({ message: "Only Admin can add Data", status: 400 });
+  }
+});
+
+app.get("/all-products", middleware, async (request, response) => {
+  const query = `SELECT * FROM product`;
+  const data = await database.all(query);
+  response.send(data);
 });
